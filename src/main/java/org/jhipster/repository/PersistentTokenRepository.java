f4f39ea019b23c8eb861e7a7291f5f4c2637899f
zhivko-georgiev <zdgeorgiev@abv.bg>
2016-05-26 18:27:23 +0300

    Initial commit

---
+++ src/main/java/org/jhipster/repository/PersistentTokenRepository.java
@@ -0,0 +1,19 @@
+package org.jhipster.repository;
+
+import org.jhipster.domain.PersistentToken;
+import org.jhipster.domain.User;
+import java.time.LocalDate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Spring Data JPA repository for the PersistentToken entity.
+ */
+public interface PersistentTokenRepository extends JpaRepository<PersistentToken, String> {
+
+    List<PersistentToken> findByUser(User user);
+
+    List<PersistentToken> findByTokenDateBefore(LocalDate localDate);
+
+}
